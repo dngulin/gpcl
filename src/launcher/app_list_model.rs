7f257acae4bfbd6e98d960e1da70dac0c8ce4545
Danil Gulin <d.nk.gulin@gmail.com>
2023-06-14 00:06:48 +0200

    Fix naming

--- src/launcher/app_list_model.rs
+++ src/launcher/app_list_model.rs
@@ -8,7 +8,7 @@ use strum::{EnumIter, FromRepr, IntoEnumIterator, IntoStaticStr};
 #[derive(Clone, Copy, FromRepr, IntoStaticStr, EnumIter)]
 #[strum(serialize_all = "snake_case")]
 #[repr(i32)]
-enum ItemField {
+enum FieldId {
     Name = qmetaobject::USER_ROLE + 1,
     Icon,
 }
@@ -16,11 +16,11 @@ enum ItemField {
 impl QmlLauncher {
     fn get_item_field(&self, index: usize, role: i32) -> Option<QVariant> {
         let item = self.launcher.items.get(index)?;
-        let field_id = ItemField::from_repr(role)?;
+        let field_id = FieldId::from_repr(role)?;
 
         let value = match field_id {
-            ItemField::Name => QString::from(item.name.as_str()).to_qvariant(),
-            ItemField::Icon => QString::from(item.icon.as_str()).to_qvariant(),
+            FieldId::Name => QString::from(item.name.as_str()).to_qvariant(),
+            FieldId::Icon => QString::from(item.icon.as_str()).to_qvariant(),
         };
 
         Some(value)
@@ -38,7 +38,7 @@ impl QAbstractListModel for QmlLauncher {
     }
 
     fn role_names(&self) -> HashMap<i32, QByteArray> {
-        let map = ItemField::iter().map(|field_id| {
+        let map = FieldId::iter().map(|field_id| {
             let field_name: &str = field_id.into();
             let key: i32 = field_id as i32;
             let val: QByteArray = field_name.into();
