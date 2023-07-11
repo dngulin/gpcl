a2977fc89653cd0943685d3961b26f95ba7d0024
Danil Gulin <d.nk.gulin@gmail.com>
2023-07-11 23:02:59 +0200

    Add screen layout configuration

--- src/config.rs
+++ src/config.rs
@@ -2,7 +2,15 @@ use serde_derive::Deserialize;
 
 #[derive(Default, Deserialize)]
 pub struct Config {
+    pub layout: Option<LayoutConfig>,
     pub items: Vec<AppIconConfig>,
+}
+
+#[derive(Deserialize)]
+pub struct LayoutConfig {
+    pub top_panel_height: Option<f32>,
+    pub clock_height: Option<f32>,
+    pub icon_size: Option<f32>,
 }
 
 #[derive(Deserialize)]
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use config::Config;
 use gamepad_manager::GamepadManager;
 use launcher::Launcher;
 
+use crate::config::{AppIconConfig, LayoutConfig};
 use slint::{SharedString, Timer, TimerMode};
 use std::cell::RefCell;
 use std::fs;
@@ -23,17 +24,48 @@ fn main() {
         .filter_level(log::LevelFilter::Debug)
         .init();
 
+    let config = load_config();
+
     winit::set_as_backend().unwrap();
     std::env::set_var("SLINT_FULLSCREEN", "1"); // Works only with winit
 
     let window = MainWindow::new().unwrap();
 
+    if let Some(layout) = &config.layout {
+        set_layout(&window, layout);
+    }
+
     let _gp_poll_timer = setup_gamepad_manager(&window);
     let _clock_timer = setup_clock(&window);
-    let _launcher_timer = setup_launcher(&window);
+    let _launcher_timer = setup_launcher(&window, &config.items);
 
     take_focus_hack(&window);
     window.run().unwrap();
+}
+
+fn load_config() -> Config {
+    let xdg_dirs = xdg::BaseDirectories::new().unwrap();
+
+    let config_path = xdg_dirs.get_config_file(CONFIG_FILE_NAME);
+    let contents = fs::read_to_string(config_path).unwrap();
+
+    toml::from_str::<Config>(&contents).unwrap()
+}
+
+fn set_layout(window: &MainWindow, layout_config: &LayoutConfig) {
+    let layout = window.global::<ScreenLayout>();
+
+    if let Some(top_panel_height) = layout_config.top_panel_height {
+        layout.set_top_panel_height(top_panel_height);
+    }
+
+    if let Some(clock_height) = layout_config.clock_height {
+        layout.set_clock_height(clock_height);
+    }
+
+    if let Some(icon_size) = layout_config.icon_size {
+        layout.set_icon_size(icon_size);
+    }
 }
 
 fn setup_gamepad_manager(window: &MainWindow) -> Timer {
@@ -72,15 +104,8 @@ fn get_time_string() -> SharedString {
     time.format("%H:%M").to_string().into()
 }
 
-fn setup_launcher(window: &MainWindow) -> Timer {
-    let xdg_dirs = xdg::BaseDirectories::new().unwrap();
-
-    let config_path = xdg_dirs.get_config_file(CONFIG_FILE_NAME);
-    let contents = fs::read_to_string(config_path).unwrap();
-
-    let config = toml::from_str::<Config>(&contents).unwrap();
-
-    let launcher = Launcher::new(&config.items);
+fn setup_launcher(window: &MainWindow, icons: &[AppIconConfig]) -> Timer {
+    let launcher = Launcher::new(icons);
     window.set_app_list(launcher.model().into());
 
     let launcher = Rc::new(RefCell::new(launcher));
